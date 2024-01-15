cd086322b5eb9f7d435b41e09a3d3202ca8486b6
Jayesh Bagde <jysh.bgde2001@gmail.com>
2024-01-15 21:08:25 +0530

    created backend server, added proxy for cors, and connected frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+import express from "express";
+app.use(express.static('dist'))
+
+const app = express()
+const port = process.env.PORT | 3000;
+
+app.get('/', (req, res)=> {
+    res.send('Hello World!')
+})
+
+app.get('/api/twitter' , (req, res) => {
+    res.send('sasukedotcom')
+})
+
+app.get('/api/login', (req, res) => {
+    res.send('<h1>App Login</h1>')
+} )
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
