8325967c0f2148d936f836deaf52dc648544deae
Jayesh Bagde <jysh.bgde2001@gmail.com>
2024-02-22 14:57:22 +0530

    deployment changes

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ if(process.env.NODE_ENV === 'production')
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-    app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname, 'client', "dist", 'index.html')))
+    app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname, 'frontend', "dist", 'index.html')))
 }
 else
 {
