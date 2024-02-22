5337b8bce34879f78594db87aa3de03dbbb67bb0
Jayesh Bagde <jysh.bgde2001@gmail.com>
2024-02-22 19:23:12 +0530

    deployment changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(cookieParser())
 if(process.env.NODE_ENV === 'production')
 {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname,'..' ,'frontend', "dist", 'index.html')))
 }
