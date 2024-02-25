8b22353d61b57845ec63fb6b47fd484eb2ad7a71
Jayesh Bagde <jysh.bgde2001@gmail.com>
2024-02-25 19:03:00 +0530

    social links

--- frontend/src/components/Footer/FooterComponent.jsx
+++ frontend/src/components/Footer/FooterComponent.jsx
@@ -78,7 +78,7 @@ const FooterComponent = () => {
                 <input type="hidden" name="redirect" value="https://web3forms.com/success?title=Subscribed%20to%20Track%20My%20Jobs&desc=Thank%20you%20for%20subscribing%20to%20Track%20My%20Jobs" />
                 <input type="hidden" name="from_name" value="Track My Jobs"></input>
                 <input type="checkbox" name="botcheck" id="" className='hidden' style={{ display: 'none' }} />
-                <Label htmlFor="subscribeEmail" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</Label>
+                <Label htmlFor="subscribe" className="block mb-2 text-sm font-medium text-gray-500 dark:text-white">Email address</Label>
                 <div className='flex'>
 
                   <TextInput type="email" id="subscribe" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" name="subscribe" required autoComplete='off' />
--- frontend/src/components/SocialLinks/SocialLinks.jsx
+++ frontend/src/components/SocialLinks/SocialLinks.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SocialLinks = () => {
   return (
-    <div>SocialLinks</div>
+    <>
+      <Link className='hover:text-green-600 font-semibold' to="https://github.com/jysh-bgde/" target='_blank' rel="noopener noreferrer">Github</Link>
+      <Link className='hover:text-green-600 font-semibold' to="https://twitter.com/bgde2001" target='_blank' rel="noopener noreferrer">Twitter</Link>
+      <Link className='hover:text-green-600 font-semibold' to="https://www.instagram.com/jayeshbagde/" target='_blank' rel="noopener noreferrer">Instagram</Link>
+    </>
   )
 }
 
